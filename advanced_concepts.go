146297a6f73e07432dd9db34dcd2571b1923ffe5
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-19 21:59:41 +0530

    Update advanced_concepts.go

--- advanced_concepts.go
+++ advanced_concepts.go
@@ -48,7 +48,62 @@ func main() {
 }
 
 
+
 // 3. Structs:
+
+package main
+
+import "fmt"
+
+type Person struct {
+    Name string
+    Age  int
+}
+
+func (p Person) SayHello() {
+    fmt.Printf("Hello, my name is %s and I am %d years old", p.Name, p.Age)
+}
+
+func main() {
+    p := Person{Name: "John", Age: 30}
+    p.SayHello() // Output: "Hello, my name is John and I am 30 years old"
+}
+
+
 
 
 // 4. Interfaces:
+
+package main
+
+import "fmt"
+
+type Shape interface {
+    Area() float64
+}
+
+type Rectangle struct {
+    Width  float64
+    Height float64
+}
+
+func (r Rectangle) Area() float64 {
+    return r.Width * r.Height
+}
+
+type Circle struct {
+    Radius float64
+}
+
+func (c Circle) Area() float64 {
+    return 3.14 * c.Radius * c.Radius
+}
+
+func main() {
+    r := Rectangle{Width: 10, Height: 20}
+    fmt.Println("Area of rectangle:", r.Area()) // 200
+
+    c := Circle{Radius: 5}
+    fmt.Println("Area of circle:", c.Area()) // 78.5
+}
+

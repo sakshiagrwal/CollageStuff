9c263320345d7025f6b1f416115d5bb3e784ea61
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-19 21:13:34 +0530

    Create basic_concepts.py
    Some examples of basic programming concepts in Python.

---
+++ basic_concepts.py
@@ -0,0 +1,67 @@
+# 1. Variables:
+
+# Declaring a variable
+x = 5
+
+# Reassigning a variable
+x = 10
+
+# Using a variable in a calculation
+y = x * 2
+
+# Printing the value of a variable
+print(y) # Output: 20
+
+# 2. Data types:
+
+# Integer
+x = 5
+
+# Float
+y = 3.14
+
+# String
+z = "Hello World"
+
+# Boolean
+a = True
+b = False
+
+# List
+my_list = [1, 2, 3, 4]
+
+# Tuple
+my_tuple = (1, 2, 3, 4)
+
+# Dictionary
+my_dict = {"a": 1, "b": 2}
+
+# 3. Loops:
+
+# For loop
+for i in range(5):
+    print(i)
+# Output: 0 1 2 3 4
+
+# While loop
+x = 0
+while x < 5:
+    print(x)
+    x += 1
+# Output: 0 1 2 3 4
+
+# 4. Conditional statements:
+
+# If statement
+x = 5
+if x > 0:
+    print("x is positive")
+# Output: "x is positive"
+
+# If-else statement
+x = -5
+if x > 0:
+    print("x is positive")
+else:
+    print("x is negative")
+# Output: "x is negative"
